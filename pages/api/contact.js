f85b99013d6b1c90925421bae8edab249c889559
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2022-10-05 21:06:59 -0300

    testing

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,37 +1,66 @@
-export default function handler(req, res) {
+import mysql from 'serverless-mysql';
+
+
+export default async function handler(req, res) {
     const body = req.body;
-    const isEmailValid = validateEmail({ email: body.email });
-    const isFNameValid = validateFName({ firstName: body.name });
-    const isLNameValid = validateLName({ fullName: body.lastName });
-    const isPWameValid = CheckPassword({ password: body.password });
+    //const isEmailValid = validateEmail({ email: body.email });
+    //const isFNameValid = validateFName({ firstName: body.name });
+    //const isLNameValid = validateLName({ fullName: body.lastName });
+    //const isPWameValid = CheckPassword({ password: body.password });
 
-    console.log("QUAL EMAIL:", body.email);
-    console.log("QUAL FIRST NAME:", body.name);
-    console.log("QUAL LAST NAME:", body.lastName);
-    console.log("QUAL PASSWORD:", body.password);
+    // console.log("QUAL EMAIL:", body.email);
+    // console.log("QUAL FIRST NAME:", body.name);
+    // console.log("QUAL LAST NAME:", body.lastName);
+    // console.log("QUAL PASSWORD:", body.password);
+
+    console.log("O QUE TA CHEGANDO AQUI:", body.subject);
+
+    // if (!isEmailValid || !isFNameValid || !isLNameValid || !isPWameValid) {
+    //     return res
+    //         .status(500)
+    //         .json({
+    //             msg: `Invalid fields: ${isEmailValid ? '' : 'email'},  ${isFNameValid ? '' : 'first name'}, ${isLNameValid ? '' : 'last name'}, ${isPWameValid ? '' : 'Wrong, try another password ...!'}`
+    //         });
+    // }
+
+    // const connection =  openConnection({ host: "ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", user: "q2w01mu95z1r9p9x", password: "b3cthljmoxgfhpyt", database: "zgwqhx5rlwm7kj0u" });
+    const connection = await openConnection({ host: "ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", user: "q2w01mu95z1r9p9x", password: "b3cthljmoxgfhpyt", database: "zgwqhx5rlwm7kj0u" });
 
 
+    console.log("CONNECTION", connection)
+    console.log("FIIIIRST NAME::::", body.subject)
 
-    if (!isEmailValid || !isFNameValid || !isLNameValid || !isPWameValid) {
-        return res
-        .status(500)
-        .json({ msg: `Invalid fields: ${isEmailValid ? '' : 'email'},  ${isFNameValid ? '' : 'first name'}, ${isLNameValid ? '' : 'last name'}, ${isPWameValid ? '' : 'Wrong, try another password ...!'}`
-    });
-    }
 
-    const connection =  openConnection({ host: "ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", user: "q2w01mu95z1r9p9x", password: "b3cthljmoxgfhpyt", database: "zgwqhx5rlwm7kj0u" });
-   
-    const haha = `SELECT * FROM users WHERE \`firstName\` = '${firstName}'`;
+ //   const haha = `SELECT * FROM persons WHERE \`FirstName\` = '${body.name}'`;
 
-    console.log("QUERY", haha);
+//   console.log("QUERY", haha);
 
-    const testing =  executeDBComand({ query: haha, connection });
-   
-    if (testing.length){
-        return "ERRO"
-    }
+    //  const testing =  executeDBComand({ query: haha, connection });
+ //   const testing = await excuteQuery({ query: haha, connection: connection });
 
-    console.log("RESULTAOD:", testing);
+ //const quizTest = `SELECT q.subject, q.question, a.quiz_id, a.description, a.is_correct FROM quiz q INNER JOIN answers a ON  a.quiz_id = q.id WHERE a.subject = '${req.body.subject}' AND a.level = '${req.body.level}'`
+ 
+ //const quizTest = `SELECT * FROM quiz WHERE subject = '${body.subject}'`;
+const quizTest = `SELECT * FROM quiz.'${body.subject}', quiz.question, answers.quiz_id, answers.description, answers.is_correct FROM quiz INNER JOIN answers ON quiz.id = 3 AND answers.quiz_id = 3`;
+
+
+//const quizTest = `SELECT * WHERE quiz.subject=${body.name}, quiz.question, answers.quiz_id FROM quiz INNER JOIN answers ON quiz.id = answers.quiz_id`;
+const quizQuery = await excuteQuery({ query: quizTest, connection: connection });
+
+    // if (testing.length){
+    //     return "ERRO"
+    // }
+
+    console.log("quizTest:", quizTest);
+
+    console.log("quizQuery:", quizQuery);
+
+   // console.log("RESULTADO:", testing);
+    await connection.end();
+
+
+    res.status(200).json(quizQuery);
+   // res.status(200).json(testing);
 
 
     //AQUIIIIIII inserir no banco de dados!!!
@@ -39,10 +68,10 @@ export default function handler(req, res) {
 
     //const result = res.json();
     //return (`Is this your full name: ${result.name}, your last name: ${result.lastName}, your email: ${result.email} and you are from ${result.country}`);
-    
+
     //return res.status(200).json({ name: `${body.name}`, lastName: `${body.lastName}`, email: `${body.email}`, password: `${body.password}`});
 
-   // return <Link href="/familytree"> It is everything ok, go to the next step! </Link>;
+    // return <Link href="/familytree"> It is everything ok, go to the next step! </Link>;
 }
 
 function validateEmail({ email }) {
@@ -56,14 +85,14 @@ function validateEmail({ email }) {
     }
 }
 
- function validateFName({ firstName }) {
+function validateFName({ firstName }) {
     if (/^[A-Za-z\s]+$/.test(firstName))
         return true;
     else
         return false;
 }
 
- function validateLName({ lastName }) {
+function validateLName({ lastName }) {
     if (/^[A-Za-z\s]+$/.test(lastName))
         return true;
     else
@@ -71,41 +100,83 @@ function validateEmail({ email }) {
 }
 
 
-function CheckPassword({ password }) 
-{ 
-var passw=  /^[A-Za-z]\w{7,14}$/;
-if(password.match(passw)) 
-{ 
-return true;
-}
-else
-{ 
-//alert('Wrong, try another password ...!')
-return false;
-}
+function CheckPassword({ password }) {
+    var passw = /^[A-Za-z]\w{7,14}$/;
+    if (password.match(passw)) {
+        return true;
+    }
+    else {
+        //alert('Wrong, try another password ...!')
+        return false;
+    }
 }
 
 
- function openConnection({ host, user, password, database }) {
-    var connection = mysql.createConnection({
-        host: host,
-        user: user,
-        password: password,
-        database: database
-    });
+//  function openConnection({ host, user, password, database }) {
+//     var connection = mysql.createConnection({
+//         host: host,
+//         user: user,
+//         password: password,
+//         database: database
+//     });
 
-    connection.connect();
-    return connection;
-}
+//     connection.connect();
+//     return connection;
+// }
 
- function executeDBComand({ query, connection }) {
-    return new Promise((resolve, reject) => {
-        connection.query(query, (error, results, fields) => {
-            if (error) {
-                return reject(error.message);
-            }
+//  function executeDBComand({ query, connection }) {
+//     return new Promise((resolve, reject) => {
+//         connection.query(query, (error, results, fields) => {
+//             if (error) {
+//                 return reject(error.message);
+//             }
 
-            return resolve(results)
-        })
+//             return resolve(results)
+//         })
+//     })
+// }
+
+
+// export default async (req, res) => {
+//     try {
+//         console.log("req nom", req.body)
+//       const result = await excuteQuery({
+//           query: 'INSERT INTO post(content) VALUES(?)',
+//           values: [req.body.content],
+//       });
+//       console.log( "ttt",result );
+//   } catch ( error ) {
+//       console.log( error );
+//   }
+
+
+//   };
+
+
+
+async function openConnection({ host, user, password, database }) {
+    const db = await mysql({
+        config: {
+            host: host,
+            database: database,
+            user: user,
+            password: password
+        }
     })
-}
+
+    db.connect();
+    return db;
+};
+
+async function excuteQuery({ query, connection }) {
+    try {
+        return await connection.query(query);
+    } catch (error) {
+        return { error };
+    }
+};
+
+
+// SELECT quiz.subject, quiz.question, answers.quiz_id
+// FROM quiz
+// INNER JOIN answers ON quiz.id=answers.quiz_id;
--- pages/clients.js
+++ pages/clients.js
@@ -6,97 +6,97 @@ export default function Contact() {
   const submitContact = async (event) => {
 
     event.preventDefault();
-    const name = event.target.firstName.value;
-    const lastName = event.target.lastName.value;
-    const email = event.target.email.value;
-    const password = event.target.password.value;
+    const subject = event.target.subject.value;
+    // const lastName = event.target.lastName.value;
+    // const email = event.target.email.value;
+    // const password = event.target.password.value;
 
     const res = await fetch('/api/contact', {
       body: JSON.stringify({
-        name: name,
-        lastName: lastName,
-        email: email,
-        password: password
+        subject: subject,
+        // lastName: lastName,
+        // email: email,
+        // password: password
       }),
       headers: {
         'Content-Type': 'application/json',
       },
       method: 'POST',
     });
-    
+
     const result = await res.json();
 
-    if (result.msg){
-      alert(result.msg)
+    if (result.msg) {
+      //alert(result.msg)
     } else {
-      alert(`Is this your full name: ${result.name}, your last name: ${result.lastName}, your email: ${result.email} and you are from ${result.password}`);
+      //alert(`Is this your full name: ${result[0].FirstName}`);
     }
   };
 
   return (
     <div className='flex items-center justify-center min-h-screen'>
       <Head>
-        <title>LOGIN</title>
+        <title>Programming</title>
         <meta name='description' content='Contact us' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <div className='max-w-xs my-2 overflow-hidden rounded shadow-lg'>
         <div className='px-6 py-4'>
-          <div className='mb-2 text-xl font-bold'>LOGIN</div>
+          <div className='mb-2 text-xl font-bold'>Type the subject</div>
           <form className='flex flex-col' onSubmit={submitContact}>
 
-          <br />
             <br />
-            
-            <label htmlFor='firstName' className='mb-2 italic'> First Name </label>
+            <br />
+
+            <label htmlFor='subject' className='mb-2 italic'> Subject </label>
             <input
               className='mb-4 border-b-2'
-              id='firstName'
-              name='firstName'
+              id='subject'
+              name='subject'
               type='text'
             //  autoComplete='firstName'
-             // required
+            // required
             />
+{/* 
+            <br />
+            <br />
 
-            <br />
-            <br />
-            
             <label htmlFor='lastName' className='mb-2 italic'> Last Name </label>
             <input
               className='mb-4 border-b-2'
               id='lastName'
               name='lastName'
               type='text'
-             // autoComplete='lastName'
-             // required
+            // autoComplete='lastName'
+            // required
             />
-            
+
             <br />
             <br />
-            
+
             <label htmlFor='email' className='mb-2 italic'> Email </label>
             <input
               className='mb-4 border-b-2'
               id='email'
               name='email'
               type='text'
-              //autoComplete='email'
+            //autoComplete='email'
             //  required
             />
-            
+
             <br />
             <br />
-            
+
             <label htmlFor='password' className='mb-2 italic'> Password </label>
             <input
               className='mb-4 border-b-2'
               id='password'
               name='password'
               type='text'
-              //autoComplete='country'
-             // required
-            />
+            //autoComplete='country'
+            // required
+            /> */}
 
             <br />
             <br />
@@ -104,12 +104,14 @@ export default function Contact() {
             <br />
             <br />
 
-            <button type='login' className='px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700'> LOGIN 
-            {/* <Link href="/familytree">LOGIN!!!</Link> */}
-             </button>
+            <button type='login' className='px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700'> LOGIN
+              {/* <Link href="/familytree">LOGIN!!!</Link> */}
+            </button>
           </form>
         </div>
       </div>
     </div>
   );
 }
+
+
